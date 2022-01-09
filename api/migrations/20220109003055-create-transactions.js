78d24112eaaccfd61991ed5daa3a99ed5804e0ef
ptbelo <ptbelo@hotmail.com>
2022-01-08 21:45:46 -0300

    feat: initial configs and starting server

---
+++ api/migrations/20220109003055-create-transactions.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      value: {
+        type: Sequelize.DOUBLE
+      },
+      sendTo: {
+        type: Sequelize.INTEGER
+      },
+      receiveFrom: {
+        type: Sequelize.INTEGER
+      },
+      currentCotation: {
+        type: Sequelize.DOUBLE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('transactions');
+  }
+};
